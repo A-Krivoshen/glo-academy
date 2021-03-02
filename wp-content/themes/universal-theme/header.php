b057c6df05782fcbd8ce8968dd68403eab1d391c
A-Krivoshein <a.krivoshein@gmail.com>
2021-03-02 16:04:58 +0300

    Домашняя работа 3

--- wp-content/themes/universal-theme/header.php
+++ wp-content/themes/universal-theme/header.php
@@ -9,5 +9,28 @@
 	
 </head>
 
-<body>
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
+<header class="header">
+  <div class="container"> 
+    <div class="header-wrapper">
+	  <?php if( has_custom_logo() ){
+	           the_custom_logo();
+               } else {
+			   echo 'Universal';
+	           }
+	   ?>
+	   <?php
+	     wp_nav_menu( [
+			'theme_location'  => 'header_menu',
+		    'container'       => 'nav', 
+			'container_class' => 'header-nav', 
+			'menu_class'      => 'header-menu', 
+			'echo'            => true,
+			 ] );  
+	   ?>
+	   <?php get_search_form(  ); ?>
+	</div>
+  </div>
+</header>
 
